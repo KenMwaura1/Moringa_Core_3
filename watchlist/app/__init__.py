64ca8b07db1eadccb944da951e76d74bc5712b1e
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-08 03:22:00 +0300

    Updated the templates to use bootstrap

--- watchlist/app/__init__.py
+++ watchlist/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from .config import Devconfig
 
 
@@ -7,7 +8,9 @@ app = Flask(__name__)
 
 # Setup the Dev config
 app.config.from_object(Devconfig)
-
 app.config.from_pyfile('instance/config.py')
+
+# Initialize Flask Extensions
+bootstrap = Bootstrap(app)
 
 from . import views
