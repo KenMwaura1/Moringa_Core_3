edbffd903554a18939f63a16f4a0a0b178f5e93e
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-09-09 11:19:17 +0300

    added tests for the Review class

---
+++ watchlist/tests/review-test.py
@@ -0,0 +1,12 @@
+import unittest
+from ..app.models import Review
+
+
+class ReviewCase(unittest.TestCase):
+    """
+    test class for the review model
+    """
+    # TODO: add tests for review
+    def test_something(self):
+        # self.assertEqual(True, False)  # add assertion here
+        pass
